1fb88ad573e09349ee33abfd36b1c012805df8cb
achmadaffandifauzan <achmadaffandifauzan@gmail.com>
2023-07-30 22:43:54 +0700

    adding user model and index.js update

--- backend/index.js
+++ backend/index.js
@@ -4,3 +4,92 @@ const path = require('path');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const mongoSanitize = require('express-mongo-sanitize');
+const ExpressError = require('./utils/ExpressError');
+const passport = require('passport');
+const LocalStrategy = require("passport-local");
+const User = require('./models/user');
+
+
+const dbUrl = process.env.DB_URL;
+const connectDB = async () => {
+    try {
+        mongoose.set('strictQuery', true);
+        const conn = await mongoose.connect(dbUrl);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+const app = express();
+
+app.engine('ejs', ejsMate);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+mongoSanitize.sanitize({
+    allowDots: true,
+    replaceWith: '_'
+});
+
+const secret = process.env.SECRET;
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    secret,
+    touchAfter: 24 * 60 * 60 // time period in seconds
+})
+store.on('error', function (e) {
+    console.log("SESSION STORE ERROR : ", e)
+})
+
+const sessionConfig = {
+    store,
+    name: 'session',
+    secret: 'asecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 2,
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}
+app.use(session(sessionConfig))
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+app.use('/', userRoutes);
+
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Not Found!', 404));
+})
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Something Went Wrong!'
+    res.status(statusCode).render('error', { err });
+})
+const PORT = process.env.PORT || 3000;
+
+
+
+//Connect to the database before listening
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT} ~express`);
+    })
+})
---
+++ backend/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    }
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', userSchema);
