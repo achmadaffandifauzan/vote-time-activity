b066ec754c5ad74c61ede4230f182e874e0231b1
achmadaffandifauzan <achmadaffandifauzan@gmail.com>
2023-08-29 19:00:50 +0700

    restructure models schema for easier management

---
+++ client/src/vote/manage/DetailVoters.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import CalendarDisplay from "../submit/CalendarDisplay";
+import Chart from "./Chart";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? window.location.origin // Use the current origin in production
+    : "http://localhost:3100"; // Use localhost in development
+const api = axios.create({
+  baseURL: baseURL,
+  withCredentials: true, // to include credentials (session cookie)
+  headers: { "Content-Type": "application/json" },
+});
+
+const calculateDetailVoters = (votingAgenda) => {
+  const detailVoters = votingAgenda.votersName.map((voterName) => {
+    return { name: voterName, votes: [] };
+  });
+  for (let votingResult of votingAgenda.votingResults) {
+    for (let key in votingResult.result) {
+      //looping through result obj
+      if (votingResult.result[key]) {
+        votingResult.result[key].foreach((voter) => {
+          if (votingAgenda.votersName.includes(voter.name)) {
+          }
+        });
+      }
+    }
+  }
+};
+const DetailVoters = ({ flashMessage, setFlashMessage, votingAgenda }) => {
+  const { voteId } = useParams();
+  const navigate = useNavigate();
+
+  return;
+};
+export default DetailVoters;
